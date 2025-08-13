e674c0f904af69529b44f25a80a8d51029c47db4
waleed <inline.development.wa@gmial.com>
2025-08-14 01:03:51 +0300

    refactor show page

--- lib/pages/show_page.dart
+++ lib/pages/show_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:metro_app/widgets/result_element.dart';
 
 class ShowPage extends StatelessWidget {
   const ShowPage({super.key});
@@ -21,42 +22,6 @@ class ShowPage extends StatelessWidget {
             ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class ResultElement extends StatelessWidget {
-  ResultElement({
-    super.key,
-    required this.text,
-    required this.result,
-    this.details = '',
-  });
-  final String text;
-  final String result;
-  final String? details;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      padding: EdgeInsets.all(8),
-      decoration: BoxDecoration(
-        color: Color(0xFFBCBCBC),
-        borderRadius: BorderRadius.circular(14),
-      ),
-      child: Column(
-        children: [
-          Text('${text + result}'),
-          SizedBox(
-            width: double.infinity,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [Text(details!)],
-            ),
-          ),
-        ],
       ),
     );
   }
